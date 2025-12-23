d62421eef6f948c68607d601ce8fbda0f11be4fe
donwheeler <slowheelerfam@gmail.com>
2025-12-22 17:39:59 -0800

    Changes to user.js post not showing

--- user.js
+++ user.js
@@ -1,28 +1,19 @@
 const postListEl = document.querySelector('.post-list');
+const id = localStorage.getItem('id');
 
 async function onSearchChange(event){
     const id = event.target.value;
+    renderPosts(id)
+}
+ 
+async function renderPosts(userId) {
     const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
     const postsData = await posts.json();
-        postListEl.innerHTML = postsData.map(post => `
-        <div class="post">
-            <div class="post__title">
-                ${post.title}
-            </div>
-                <p class="post__body">
-                ${post.body}
-                </p>
-        </div>
-    `).join('');
+    postListEl.innerHTML = postsData.map(post => postHTML(post)).join('');
 }
 
-async function main(){
-    const id = localStorage.getItem('id');
-    const posts = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-    const postsData = await posts.json();
-    console.log(postsData);
-
-    postListEl.innerHTML = postsData.map(post => `
+function postHTML(post) {
+    return `
         <div class="post">
             <div class="post__title">
                 ${post.title}
@@ -31,7 +22,7 @@ async function main(){
                 ${post.body}
                 </p>
         </div>
-    `).join('');
+    `
     }
  
-main(); 
+renderPosts(id); 
